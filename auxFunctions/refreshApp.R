bc4aa7ccc415eaaf507aa58d828fc803a4669691
theasjblog <tequilacanaria@hotmail.it>
2021-02-13 13:16:50 +0000

    file fix

--- auxFunctions/refreshApp.R
+++ auxFunctions/refreshApp.R
@@ -1,14 +1,14 @@
 source('dependencies.R')
 saveAllData <- function(branchName = 'master'){
-# remove old version of covid19Visualizer ---------------------------------
+  # remove old version of covid19Visualizer ---------------------------------
   listFiles <- list.files('./auxFunctions')
   for (i in listFiles){
     if (i != 'refreshApp.R'){
       file.remove(paste0('./auxFunctions/', i))
     }
   }
-
-# get newest version of covid19Visualizer ---------------------------------
+  
+  # get newest version of covid19Visualizer ---------------------------------
   # ideally we deploy installing the package,
   # but shinyapps.io fails to do that
   download.file(url = paste0("https://github.com/theasjblog/covid19_package/archive/",branchName,".zip")
